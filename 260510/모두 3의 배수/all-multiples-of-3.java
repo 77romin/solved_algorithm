041df82fb4a471fdd08e7b5eee6112454a035da3
Kangmin Kim <bighead0831@naver.com>
2026-05-10 23:31:55 +0900

    [Passed] 모두 3의 배수 (129ms, 10MB)

---
+++ 260510/모두 3의 배수/all-multiples-of-3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int chk = 1;
+        for(int i=0; i<5; i++) {
+            int n = sc.nextInt();
+            if(n%3!=0) {
+                chk = 0;
+            }
+        }
+        System.out.println(chk);
+
+
+    }
+}
