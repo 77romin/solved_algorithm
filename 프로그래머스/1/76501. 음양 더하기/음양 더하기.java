20d960a8d3b7ec43ef9069f8485f296536a8e71f
Max Kim <bighead0831@naver.com>
2026-08-07 08:34:18 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum = 0;
+        for(int i=0; i<absolutes.length; i++) {
+            absolutes[i] *= signs[i]?1:-1;
+            sum += absolutes[i];
+        }
+        
+        return sum;
+    }
+}
