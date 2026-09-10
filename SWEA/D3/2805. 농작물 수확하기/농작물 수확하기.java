bcb9dc067a9501943ffdfa14af7b1d6bf7ba0fb2
Max Kim <bighead0831@naver.com>
2026-09-10 10:12:30 +0900

    [D3] Title: 농작물 수확하기, Time: 138 ms, Memory: 35,200 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    private static int N;
+    private static int[][] farm;
+    
+    private static int center;
+    
+    private static int crops;
+    
+	public static void main(String args[]) throws Exception {
+		int T = Integer.parseInt(br.readLine().trim());
+		StringBuilder sb = new StringBuilder();
+        
+		for(int test_case = 1; test_case <= T; test_case++) {
+            init();
+            harvest();
+            
+            sb.append("#").append(test_case).append(" ")
+                .append(crops).append("\n");
+		}
+        System.out.print(sb);
+	}
+    
+    private static void harvest() {
+        int sp = center;
+        int ep = center;
+        
+         for(int i=0; i<N; i++) {
+             for(int j=sp; j<=ep; j++) {
+                 crops += farm[i][j];
+             }
+
+             if(i<center) {
+                 sp -= 1;
+                 ep += 1;
+             } else {
+                 sp += 1;
+                 ep -= 1;
+             }
+             
+        }
+    }
+    
+    private static void init() throws IOException {
+        crops = 0;
+        N = Integer.parseInt(br.readLine().trim());
+        farm = new int[N][N];
+        for(int i=0; i<N; i++) {
+            String[] oneLine = br.readLine().trim().split("", N);
+            for(int j=0; j<N; j++) {
+                farm[i][j] = Integer.parseInt(oneLine[j]);
+            }
+        }
+        center = (N-1)/2;
+    }
+}
