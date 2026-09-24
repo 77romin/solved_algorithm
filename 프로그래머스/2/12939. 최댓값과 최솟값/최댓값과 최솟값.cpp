2d97d36ccb726181b14b48067270c692678e0ece
Max Kim <bighead0831@naver.com>
2026-09-24 16:07:43 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.02 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string s) {
+    stringstream ss(s);
+    int num;
+    
+    ss >> num;
+    int min_val = num;
+    int max_val = num;
+    
+    while(ss>>num) {
+        min_val = min(min_val, num);
+        max_val = max(max_val, num);
+    }
+    
+    return to_string(min_val) + " " + to_string(max_val);
+}
