a34ad1a224210168e5f1d5d84e88390a65bdfd16
Max Kim <bighead0831@naver.com>
2026-09-25 13:50:52 +0900

    [level 2] Title: 피보나치 수, Time: 0.50 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    vector<int> f(n+1, 0);
+    f[1] = 1;
+    for(int i=2; i<=n; i++) {
+        f[i] = (f[i-1]%1234567+f[i-2]%1234567)%1234567;
+    }
+    return f[n];
+}
