b84cd7500ed1117903b4730aae9fdd84dddd3460
Max Kim <bighead0831@naver.com>
2026-09-25 12:53:09 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = string(phone_number.length()-4, '*');
+    answer.append(phone_number.substr(phone_number.length()-4));
+    return answer;
+}
