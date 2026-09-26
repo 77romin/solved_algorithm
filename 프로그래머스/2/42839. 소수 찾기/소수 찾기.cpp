800f731b5c5e05b337c7cb52bef1764fca2937bd
Max Kim <bighead0831@naver.com>
2026-09-27 01:07:01 +0900

    [level 2] Title: 소수 찾기, Time: 0.43 ms, Memory: 4.92 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,39 +1,39 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 unordered_set<int> unique_nums;
-bool visited[7]; // numbers는 1이상 7이하의 문자열
+bool visited[8];
 
 bool isPrime(int n) {
     if(n<2) return false;
     for(int i=2; i<=sqrt(n); i++) {
-        if(n%i == 0) return false;
+        if(n%i==0) return false;
     }
     return true;
 }
 
 void dfs(string numbers, string current) {
-    if(!current.empty()) {
+    if(!current.empty())
         unique_nums.insert(stoi(current));
-    }
     
     for(int i=0; i<numbers.length(); i++) {
-        if(!visited[i]) {
-            visited[i] = true;
-            dfs(numbers, current+numbers[i]);
-            visited[i] = false;
-        }
+        if(visited[i]) continue;
+        visited[i] = true;
+        dfs(numbers, current+numbers[i]);
+        visited[i] = false;
     }
 }
 
+
+
 int solution(string numbers) {
-    unique_nums.clear();
+    int answer = 0;
     
     dfs(numbers, "");
     
-    int answer = 0;
-    for(int num : unique_nums) {
-        if(isPrime(num)) answer++;
+    for(int n : unique_nums) {
+        answer = isPrime(n) ? answer+1 : answer;
     }
     
     return answer;
